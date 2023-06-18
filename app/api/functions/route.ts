c5f073931b697f79a89838c6efeb9f35b7a28cca
Dennis Cual <denniscual0716@gmail.com>
2023-06-19 04:33:24 +0800

    include converation context to get the result

--- app/api/functions/route.ts
+++ app/api/functions/route.ts
@@ -14,14 +14,15 @@ export async function POST(req: Request) {
   const response = await model.createChatCompletion({
     model: 'gpt-3.5-turbo-0613',
     messages,
-    functions: [functionDescription],
+    functions: functionsDefinitions,
     function_call: 'auto',
   });
 
   const message = response?.data?.choices?.[0]?.message;
 
-  if (message?.function_call && message.function_call.arguments) {
-    const functionResponse = await get_user_tasks(JSON.parse(message.function_call.arguments));
+  if (message?.function_call && message?.function_call.name && message.function_call.arguments) {
+    const foundFunctions = functions[message.function_call.name];
+    const functionResponse = await foundFunctions(JSON.parse(message.function_call.arguments));
 
     const response = await model.createChatCompletion({
       model: 'gpt-3.5-turbo-0613',
@@ -44,25 +45,52 @@ export async function POST(req: Request) {
   return NextResponse.json(message?.content);
 }
 
-interface Function {
+const functionsDefinitions: {
   name: string;
   description: string;
   parameters: object;
-}
-
-const functionDescription: Function = {
-  name: 'get_user_tasks',
-  description: 'Get the current tasks of a user.',
-  parameters: {
-    type: 'object',
-    properties: {
-      userId: {
-        type: 'string',
-        description: 'The id of the user.',
+}[] = [
+  {
+    name: 'get_user_tasks',
+    description: 'Get the current tasks of a user.',
+    parameters: {
+      type: 'object',
+      properties: {
+        userId: {
+          type: 'string',
+          description: 'The id of the user.',
+        },
       },
+      required: ['userId'],
     },
-    required: ['userId'],
   },
+  {
+    name: 'create_user_task',
+    description: 'Create a task for a user',
+    parameters: {
+      type: 'object',
+      properties: {
+        userId: {
+          type: 'string',
+          description: 'The id of the user.',
+        },
+        title: {
+          type: 'string',
+          description: 'The title of the task.',
+        },
+        description: {
+          type: 'string',
+          description: 'Description of the task',
+        },
+      },
+      required: ['userId', 'title'],
+    },
+  },
+];
+
+const functions: Record<string, (args: any) => Promise<any>> = {
+  get_user_tasks,
+  create_user_task,
 };
 
 async function get_user_tasks({ userId }: { userId: string }) {
@@ -71,4 +99,22 @@ async function get_user_tasks({ userId }: { userId: string }) {
     .from(task)
     .where(eq(task.userId, parseInt(userId)));
   return JSON.stringify(tasks);
+}
+
+async function create_user_task({
+  userId,
+  title,
+  description,
+}: {
+  userId: string;
+  title: string;
+  description: string;
+}) {
+  const newTask = {
+    userId: parseInt(userId),
+    title,
+    description,
+  };
+  await db.insert(task).values(newTask);
+  return JSON.stringify(newTask);
 }
