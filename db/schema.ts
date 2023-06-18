36edeaf50f7da7223bfe3d8107cbec64844a85a0
Dennis Cual <denniscual0716@gmail.com>
2023-06-18 20:56:52 +0800

    add index to task table

--- db/schema.ts
+++ db/schema.ts
@@ -1,16 +1,33 @@
-import { mysqlTable, int, varchar, text, date, mysqlEnum, timestamp } from 'drizzle-orm/mysql-core';
+import {
+  mysqlTable,
+  int,
+  index,
+  varchar,
+  text,
+  date,
+  mysqlEnum,
+  timestamp,
+} from 'drizzle-orm/mysql-core';
 import { InferColumnsDataTypes } from 'drizzle-orm';
 
-export const task = mysqlTable('task', {
-  id: int('id').autoincrement().primaryKey().notNull(),
-  userId: int('user_id').notNull(),
-  title: varchar('title', { length: 255 }).notNull(),
-  description: text('description'),
-  dueDate: date('due_date', { mode: 'date' }),
-  status: mysqlEnum('status', ['pending', 'completed']).default('pending'),
-  createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow().onUpdateNow(),
-});
+export const task = mysqlTable(
+  'task',
+  {
+    id: int('id').autoincrement().primaryKey().notNull(),
+    userId: int('user_id').notNull(),
+    title: varchar('title', { length: 255 }).notNull(),
+    description: text('description'),
+    dueDate: date('due_date', { mode: 'date' }),
+    status: mysqlEnum('status', ['pending', 'completed']).default('pending'),
+    createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
+    updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow().onUpdateNow(),
+  },
+  (table) => {
+    return {
+      titleIdx: index('title_idx').on(table.title),
+    };
+  }
+);
 
 export const user = mysqlTable('user', {
   id: int('id').autoincrement().primaryKey().notNull(),
