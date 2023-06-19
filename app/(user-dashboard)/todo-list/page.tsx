26341ae459995f8fb212cb4dd6b3a2728714713c
Dennis Cual <denniscual0716@gmail.com>
2023-06-20 02:50:16 +0800

    add chat loader

--- app/(user-dashboard)/todo-list/page.tsx
+++ app/(user-dashboard)/todo-list/page.tsx
@@ -2,7 +2,7 @@
 import { useChat } from 'ai/react';
 
 export default function TodoListPage() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat({
     api: '/api/functions',
   });
 
@@ -22,12 +22,15 @@ export default function TodoListPage() {
             ))
           : null}
         <form onSubmit={handleSubmit}>
-          <input
-            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-            value={input}
-            placeholder="Retrieve all of my todos..."
-            onChange={handleInputChange}
-          />
+          <div className="fixed bottom-0 w-full">
+            <input
+              className="w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl "
+              value={input}
+              placeholder="Retrieve all of my todos..."
+              onChange={handleInputChange}
+            />
+            {isLoading && <span className="inline-block ml-4">Loading...</span>}
+          </div>
         </form>
       </div>
     </div>
