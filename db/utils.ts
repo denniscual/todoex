9c59dfaaf33cfc600e27d82ff42f32e835d1053d
Dennis Cual <denniscual0716@gmail.com>
2023-06-22 23:13:56 +0800

    add upsertUser util

--- db/utils.ts
+++ db/utils.ts
@@ -1,6 +1,6 @@
 import 'server-only';
 import { cache } from 'react';
-import { db, task } from '@/db';
+import { db, task, User, user } from '@/db';
 import { eq } from 'drizzle-orm';
 
 export const getUserTasks = cache(async (id: number = 1) => {
@@ -8,7 +8,7 @@ export const getUserTasks = cache(async (id: number = 1) => {
   return tasks;
 });
 
-export const insertTaskById = async ({
+export async function insertTaskById({
   id,
   title,
   description,
@@ -16,10 +16,18 @@ export const insertTaskById = async ({
   id: number;
   title: string;
   description: string;
-}) => {
+}) {
   await db.insert(task).values({
     userId: id,
     title,
     description,
   });
-};
+}
+
+export async function upsertUser(value: User) {
+  await db
+    .insert(user)
+    .values(value)
+    // @ts-expect-error - onConflictDoUpdate is not in the types yet.
+    .onConflictDoUpdate({ target: user.id, set: { name: 'John' } });
+}
