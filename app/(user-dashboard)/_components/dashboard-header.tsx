b37804ed0dbe3216ce39697be937d1fdea18d9c2
Dennis Cual <denniscual0716@gmail.com>
2023-07-15 03:25:38 +0800

    add dashboard main nav

--- app/(user-dashboard)/_components/dashboard-header.tsx
+++ app/(user-dashboard)/_components/dashboard-header.tsx
@@ -1,15 +1,16 @@
 import { UserButton } from '@clerk/nextjs';
 import { ThemeToggle } from '@/components/theme-toggle';
-import { Suspense } from 'react';
+import DashboadMainNav from './dashboard-main-nav';
 
 export default function DashboardHeader() {
   return (
-    <header className="w-full border-b supports-backdrop-blur:bg-background/60 bg-background/95 backdrop-blur">
+    <header className="w-full py-2 border-b">
       <div className="container flex items-center h-14">
-        <div>Main nav</div>
-        <div>Mobile nav</div>
+        <DashboadMainNav />
+        {/* Add here the mobile nav */}
+        <div />
         <div className="flex items-center justify-between flex-1 space-x-2 md:justify-end">
-          <nav className="flex items-center gap-x-6">
+          <nav className="flex items-center gap-x-4">
             <ThemeToggle />
             <UserButton afterSignOutUrl={`${process.env.NEXT_PUBLIC_HOST}/sign-in`} />
           </nav>
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'Todoex',
+  url: '',
+  ogImage: '',
+  description: `Boost productivity with Todoex, the smart todo app powered by AI. Stay organized and set reminders to 
+     optimize your task management. Embrace the future of productivity.`,
+  links: {
+    github: 'https://github.com/denniscual/todoex',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
