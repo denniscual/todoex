8fe8975945719b8c0623ef5913f737f531742b3a
Dennis Cual <denniscual0716@gmail.com>
2023-07-15 01:08:49 +0800

    create providers file, move the ClerkProvider, and add ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
 import { fontSans } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
+import { AppThemeProvider, AppClerkProvider } from '@/components/providers';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <ClerkProvider>{children}</ClerkProvider>
+        <AppThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <AppClerkProvider>{children}</AppClerkProvider>
+        </AppThemeProvider>
       </body>
     </html>
   );
