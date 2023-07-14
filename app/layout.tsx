d3963be8f1ff7fbb134621df93ce727610df0a30
Dennis Cual <denniscual0716@gmail.com>
2023-07-14 22:18:55 +0800

    add fonts utils and update font config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-});
+import { fontSans } from '@/lib/fonts';
+import { cn } from '@/lib/utils';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body className="antialiased text-black bg-white">
+    <html lang="en">
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         <ClerkProvider>{children}</ClerkProvider>
       </body>
     </html>
