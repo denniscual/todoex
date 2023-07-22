4f9f8bf3d0b97459745a6dd2cba5be4a82fc29c9
Dennis Cual <denniscual0716@gmail.com>
2023-07-22 14:13:47 +0800

    show user task details in the task page

--- app/(user-dashboard)/tasks/[id]/page.tsx
+++ app/(user-dashboard)/tasks/[id]/page.tsx
@@ -1,3 +1,25 @@
-export default function Task() {
-  return <div>Task page</div>;
+import { getTaskById } from '@/db';
+import UserTaskDetails from '@/app/(user-dashboard)/_components/user-task-details';
+import { Card, CardContent } from '@/components/ui/card';
+
+export default async function Task({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const userTask = await getTaskById(parseInt(params.id));
+
+  if (!userTask) {
+    return <div>No task found.</div>;
+  }
+
+  return (
+    <Card>
+      <CardContent className="p-0">
+        <UserTaskDetails id={parseInt(params.id)} />
+      </CardContent>
+    </Card>
+  );
 }
