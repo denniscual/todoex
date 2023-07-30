e88d1ba710c8ef65452e91f8fcf995930d316e6d
Dennis Cual <denniscual0716@gmail.com>
2023-07-30 23:19:57 +0800

    create an actionWrapper to extend the functionality of the action

--- lib/actions.ts
+++ lib/actions.ts
@@ -15,7 +15,7 @@ export type InsertTaskAction = (task: z.infer<typeof insertTaskSchema>) => Promi
   };
 }>;
 
-export const insertTaskAction: InsertTaskAction = async function (task) {
+export const insertTaskAction: InsertTaskAction = actionWrapper(async function (task) {
   try {
     await insertTask(task);
     return {
@@ -32,7 +32,7 @@ export const insertTaskAction: InsertTaskAction = async function (task) {
       throw new Error('Server error');
     }
   }
-};
+});
 
 export type UpdateTaskByIdAction = (task: TaskWithProject) => Promise<{
   result: {
@@ -40,51 +40,51 @@ export type UpdateTaskByIdAction = (task: TaskWithProject) => Promise<{
   } & TaskWithProject;
 }>;
 
-export const updateTaskByIdAction: UpdateTaskByIdAction = async function (task) {
-  try {
-    await updateTaskById({
-      id: task.id,
-      status: task.status,
-      dueDate: task.dueDate,
-      title: task.title,
-      description: task.description,
-      projectId: task.projectId,
-      // TODO:
-      // - change to its correct type
-      content: task.content as any,
-    });
-    return {
-      result: {
-        message: 'Task status is updated successfully.',
-        ...task,
-      },
-    };
-  } catch (err) {
-    if (err instanceof ZodError) {
-      console.log('Zod error: ', err.issues);
-      throw new Error(JSON.stringify(err.issues));
-    } else {
-      console.log('Error when generating response: ', (err as Error).toString());
-      throw new Error('Server error');
-    }
-  }
-};
+export const updateTaskByIdAction: UpdateTaskByIdAction = actionWrapper(async function (task) {
+  await updateTaskById({
+    id: task.id,
+    status: task.status,
+    dueDate: task.dueDate,
+    title: task.title,
+    description: task.description,
+    projectId: task.projectId,
+    // TODO:
+    // - change to its correct type
+    content: task.content as any,
+  });
+  return {
+    result: {
+      message: 'Task status is updated successfully.',
+      ...task,
+    },
+  };
+});
 
-export const deleteTaskByIdAction = async function (id: Task['id']) {
-  try {
-    await deleteTaskById(id);
-    return {
-      result: {
-        message: 'Task status is delete successfully.',
-      },
-    };
-  } catch (err) {
-    if (err instanceof ZodError) {
-      console.log('Zod error: ', err.issues);
-      throw new Error(JSON.stringify(err.issues));
-    } else {
-      console.log('Error when generating response: ', (err as Error).toString());
-      throw new Error('Server error');
+export const deleteTaskByIdAction = actionWrapper(async function (id: Task['id']) {
+  await deleteTaskById(id);
+  return {
+    result: {
+      message: 'Task status is delete successfully.',
+    },
+  };
+});
+
+/**
+ * A Wrapper function that extends the passed `action`. E.g it handles the error
+ * message throws by the `action.
+ */
+function actionWrapper<T extends (...args: any[]) => Promise<any>>(action: T): T {
+  return (async (...args: Parameters<T>): Promise<ReturnType<T>> => {
+    try {
+      return await action(...args);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        console.log('Zod error: ', err.issues);
+        throw new Error(JSON.stringify(err.issues));
+      } else {
+        console.log('Error when generating response: ', (err as Error).toString());
+        throw new Error('Server error');
+      }
     }
-  }
-};
+  }) as T;
+}
