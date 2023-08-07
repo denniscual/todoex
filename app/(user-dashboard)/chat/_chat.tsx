ab4c92fca0fe84f461bc473d0da066935164fefd
Dennis Cual <denniscual0716@gmail.com>
2023-08-08 00:56:17 +0800

    create ChatMessage layout for user and assistant role

--- app/(user-dashboard)/chat/_chat.tsx
+++ app/(user-dashboard)/chat/_chat.tsx
@@ -1,10 +1,13 @@
 'use client';
-import { Separator } from '@/components/ui/separator';
 import { OpenAIIcon } from '@/components/ui/icons';
 import { ChatScrollAnchor } from '@/components/chat-scroll-anchor';
 import ChatPanel from './_chat-panel';
-import { PropsWithChildren } from 'react';
+import { HTMLAttributes, PropsWithChildren, ReactNode } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { Card, CardContent } from '@/components/ui/card';
+import { PersonIcon } from '@radix-ui/react-icons';
+import { ChatCompletionRequestMessage } from 'openai';
+import { cn } from '@/lib/utils';
 
 export default function Chat({
   initialMessage,
@@ -23,18 +26,33 @@ export default function Chat({
         <div className="mb-12">{messages.filter(Boolean).length === 0 && children}</div>
         {enableChat && (
           <>
-            <ul>
-              {[initialMessage].map((val, idx) => (
-                <li key={idx}>
-                  <div className="relative flex items-start mb-4">
-                    <span className="flex items-center justify-center w-8 h-8 bg-green-400 border rounded-md shadow select-none dark:bg-green-600 shrink-0">
-                      <OpenAIIcon className="w-4 h-4 text-white" />
-                    </span>
-                    <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">{val}</div>
-                  </div>
-                  <Separator className="my-4 md:my-8" />
-                </li>
-              ))}
+            <ul className="space-y-4 md:space-y-8">
+              <li key={0}>
+                <ChatMessage
+                  icon={
+                    <ChatIcon className="bg-primary">
+                      <PersonIcon className="w-4 h-4 text-primary-foreground" />
+                    </ChatIcon>
+                  }
+                >
+                  hello world
+                </ChatMessage>
+              </li>
+              <li>
+                <Card>
+                  <CardContent className="px-0 pt-6">
+                    <ChatMessage
+                      icon={
+                        <ChatIcon className="bg-green-400 dark:bg-green-600">
+                          <OpenAIIcon className="w-4 h-4 text-white" />
+                        </ChatIcon>
+                      }
+                    >
+                      Hello world
+                    </ChatMessage>
+                  </CardContent>
+                </Card>
+              </li>
             </ul>
             <ChatScrollAnchor trackVisibility={false} />
           </>
@@ -42,5 +60,32 @@ export default function Chat({
       </div>
       {enableChat && <ChatPanel />}
     </>
+  );
+}
+
+function ChatMessage({ icon, children }: PropsWithChildren<{ icon?: ReactNode }>) {
+  return (
+    <div className="flex items-start px-6">
+      {icon}
+      <div className="flex-1 px-1 ml-4">{children}</div>
+    </div>
+  );
+}
+
+function ChatIcon({
+  children,
+  className,
+  ...props
+}: PropsWithChildren<HTMLAttributes<HTMLSpanElement>>) {
+  return (
+    <span
+      {...props}
+      className={cn(
+        'flex items-center justify-center w-8 h-8 border rounded-md shadow select-none shrink-0',
+        className
+      )}
+    >
+      {children}
+    </span>
   );
 }
