e407780593c70ec5dc477470e647378cba351d89
Dennis Cual <denniscual0716@gmail.com>
2023-08-07 23:29:57 +0800

    create a barebone index page

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -1,8 +1,3 @@
 export default function IndexLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <h1>Index layout</h1>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
